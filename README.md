ae3172a6f23be93c39202d88237ce66f37c7deef
dagreatcode <dagreatcode@gmail.com>
2023-08-06 15:16:09 -0400

    updaating the bones of this site added admin app also

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # GADZ
+
+npx create-react-app pwa-react-typescript --template cra-template-pwa-typescript
+https://github.com/marmelab/react-admin
+sudo pkill -f node
---
+++ admin/src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { render } from 'react-dom';
+import { Admin, Resource } from 'react-admin';
+import restProvider from 'ra-data-simple-rest';
+
+import { PostList, PostEdit, PostCreate, PostIcon } from './posts.js';
+
+
+function App() {
+  return (
+    <Admin dataProvider={restProvider('http://localhost:3000')}>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+    </Admin>
+  );
+}
+
+export default App;
