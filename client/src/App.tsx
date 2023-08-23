a721f30d503d5c67544fa24543767c14824f0845
dagreatcode <dagreatcode@gmail.com>
2023-08-22 23:51:31 -0400

    updated to mysql. will upgrade to sql2..tbd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,25 @@
-// import * as React from "react";
-// import { render } from 'react-dom';
-import { Admin, Resource } from 'react-admin';
-import restProvider from 'ra-data-simple-rest';
-
-import { PostList, PostEdit, PostCreate, PostIcon } from './posts.js';
-
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Admin dataProvider={restProvider('http://localhost:3000')}>
-        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
-    </Admin>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- server.js
+++ server.js
@@ -1,66 +1,74 @@
+// *********************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
 require("dotenv").config();
 const express = require("express");
-// const mongoose = require("mongoose");
 const path = require("path");
-// const UserController = require("./controllers/UsersController");
-// const AuthController = require("./controllers/authController");
+const mysql = require("mysql");
+// const Sequelize = require("sequelize"); // Upgrade to MySQL2
+// const inquirer = require("inquirer"); // Create Console App
+// TODO: upgrade to MySQL2, Create Console App, Finish & Divided out routes and models
 
+// Set server PORT
+// =============================================================
 const PORT = process.env.PORT || 3001;
 
+// Sets up the Express App
+// =============================================================
 const app = express();
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
+// Serve up static assets (usually on heroku/render)
+// =============================================================
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
 }
-// mongoose.connect(
-// 	process.env.MONGODB_URI || "mongodb://localhost/gadz_db",
-// 	{
-// 		useNewUrlParser: true,
-// 		useUnifiedTopology: true,
-// 		// useCreateIndex: true,
-// 		// useFindAndModify: false,
-// 		// onAfterSetupMiddleware: true,
-// 		// onBeforeSetupMiddleware: true,
-// 	}
-// );
-
-// const connection = mongoose.connection;
-
-// connection.on("connected", () => {
-// 	console.log("Mongoose successfully connected.");
-// });
-
-// connection.on("error", (err) => {
-// 	console.log("Mongoose connection error: ", err);
-// });
-
+// Connect to SQL Database
+// =============================================================
+var connection = mysql.createConnection({
+	// Your host
+  host: process.env.dbHOST,
+  // Your port; if not 3306
+  port: process.env.dbPORT,
+  // Your username
+  user: process.env.dbUSER,
+  // Your password
+  password: process.env.dbPASSWORD,
+  database: process.env.dbDATABASE
+});
+connection.connect(function(err) {
+console.log("connected")
+});
 // // Outside Routes
 // app.use("/api/user", UserController);
 // app.use(AuthController);
-
+// require("./routes/post-api-routes.js")(app);
 // TODO: Add console app.
 
+// Test routes to see if ther server is talking to the client
+// =============================================================
 app.get("/api/config", (req, res) => {
 	res.json({
 		success: true,
 	});
 });
-
 app.get("/apiFun", (req, res) => {
 	res.send("API FUN");
 	var adminUser = req.params.apiFun;
 	console.log(adminUser);
 	res.end();
 });
-
+// Used as a wildcard if something goes wrong
+// =============================================================
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-
+// Starts the server to begin listening
+// =============================================================
 app.listen(PORT, () => {
 	console.log(`🌎 App is running on http://localhost:${PORT}`);
 });
