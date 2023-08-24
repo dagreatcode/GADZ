dbb1e4c707e8ff38511baf05aae886f0f7ecf8b2
dagreatcode <dagreatcode@gmail.com>
2023-08-24 19:22:27 -0400

    added the wireframe and updated the banner

--- client/src/components/Banner/Banner.tsx
+++ client/src/components/Banner/Banner.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 function Banner() {
   return (
     <>
-      <iframe 
+      {/* <iframe 
         width={1222} height={887}
         src="https://www.youtube.com/embed/ErOMbspikNg" 
         title="4K 3D Earth Globe Spinning" 
         //   frameBorder="0" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture full" 
         allowFullScreen
-      ></iframe>
+      ></iframe> */}
     </>
   )
 }
