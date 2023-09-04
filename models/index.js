129b36193aeed3173cafbf4dee7f372c4280cdea
dagreatcode <dagreatcode@gmail.com>
2023-09-04 02:33:55 -0400

    all over the place connecting the postgres sequelize

--- models/index.js
+++ models/index.js
@@ -15,12 +15,12 @@ if (config.use_env_variable) {
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-// try {
-// 	sequelize.authenticate();
-// 	console.log('Connection has been established successfully.');
-//   } catch (error) {
-// 	console.error('Unable to connect to the database:', error);
-//   }
+try {
+	sequelize.authenticate();
+	console.log('Connection has been established successfully.');
+  } catch (error) {
+	console.error('Unable to connect to the database:', error);
+  }
 
 fs
   .readdirSync(__dirname)
