386b0afd54c6a3dde355f807298aaadbfa6e2859
dagreatcode <dagreatcode@gmail.com>
2023-09-09 18:15:32 -0400

    added the api link to the users database

--- client/src/containers/Admin/Users/UserProfile/UserProfile.tsx
+++ client/src/containers/Admin/Users/UserProfile/UserProfile.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const UserProfile = () => {
   return (
-    <div>UserProfile</div>
+    <>
+      <div>UserProfile</div>
+      <a href="https://gadzconnect.com/api/user/AllUsers">Users API. This is Raw Data coming from the database. This will be user to apply tho this page for the admin to adit user or freeze user."</a>        
+    </>
   )
 }
 
