1c47dfe373d666ce89b9bef32d1f66b7ac096b82
dagreatcode <dagreatcode@gmail.com>
2023-09-09 19:34:45 -0400

    Added meta data3

--- client/src/containers/Admin/Users/AllUsers.tsx
+++ client/src/containers/Admin/Users/AllUsers.tsx
@@ -5,7 +5,7 @@ const AllUsers = () => {
   return (
     <>
       <div>AllUsers</div>
-     <Link to="/AdminUserProfile">Click to edit single user, change information, approve business to user the site + credit, or archive user.</Link><br/><br/>
+     <Link to="/AdminUserProfile">Click to edit single user, change information, approve business to use the site + credit, or archive user.</Link><br/><br/>
      <a href="https://gadzconnect.com/api/user/AllUsers">Users API. This is Raw Data coming from the database. This will be user to apply tho this page for the admin to adit user or freeze user."</a>        
     </>
   )
