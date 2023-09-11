388f75ab6a59d29cadef21afa7f7a5e83a17a970
dagreatcode <dagreatcode@gmail.com>
2023-09-10 20:59:42 -0400

    added redux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
+// import React, { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   // Navigate,
 } from "react-router-dom";
-
+// import { useDispatch, useSelector } from "react-redux";
+// import { getArticles, getArticlesById } from "./redux/actions/index";
 import Home from "./containers/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
 import NotFound from "./containers/NotFound/NotFound";
@@ -34,6 +36,32 @@ import AdminUserProfile from "./containers/Admin/Users/UserProfile/UserProfile"
 
 
 function App() {
+
+  //   // const posts = useSelector((state) => state.articles);
+  // // const myPosts = useSelector((state) => state.myArticles);
+  // const error = useSelector((state) => state.error);
+  // const loading = useSelector((state) => state.loading);
+  // // const reload = useSelector((state) => state.reload);
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(getArticles());
+  //   // return () => {};
+  // }, [dispatch]);
+
+  // useEffect(() => {
+  //   dispatch(getArticlesById());
+  //   // return () => {};
+  // }, [dispatch]);
+
+  // if (error) {
+  //   return <h1>{error}</h1>;
+  // }
+
+  // if (loading) {
+  //   return <h1>Loading . . .</h1>;
+  // }
+  
   return (
     <Router>
        <NavBar />
