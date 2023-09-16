5ec9b7c479d4e2b90dbdf36db0718b96e77d7822
dagreatcode <dagreatcode@gmail.com>
2023-09-16 01:57:02 -0400

    updating the Admin page.

--- client/src/containers/Admin/Users/AllUsers.tsx
+++ client/src/containers/Admin/Users/AllUsers.tsx
@@ -5,8 +5,10 @@ const AllUsers = () => {
   return (
     <>
       <div>AllUsers</div>
+      <h1>These links are to make sure we can navigate to all of the pages created through out the site. </h1>
+      <h5> I will create a login  with authorization and authentication for Admin's and User's momentarily</h5>
      <Link to="/AdminUserProfile">Click to edit single user, change information, approve business to use the site + credit, or archive user.</Link><br/><br/>
-     <a href="https://gadzconnect.com/api/user/AllUsers">Users API. This is Raw Data coming from the database. This will be user to apply tho this page for the admin to adit user or freeze user."</a>        
+     <a href="https://gadzconnect.com/api/user/AllUsers">Users API. This is Raw Data coming from the database. This will be user to apply all Users Information to the page for the admin edit user or freeze user or even archive."</a>        
     </>
   )
 }
