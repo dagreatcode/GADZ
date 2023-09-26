ad737cb38f2ce58175f81378d7bee80c497ef69e
dagreatcode <dagreatcode@gmail.com>
2023-09-26 15:31:53 -0400

    updating the frontent with forms userProfile page

--- client/src/containers/User/Profile/ProfileUpdate.tsx
+++ client/src/containers/User/Profile/ProfileUpdate.tsx
@@ -2,7 +2,123 @@ import React from 'react'
 
 const ProfileUpdate = () => {
   return (
-    <div>ProfileUpdate</div>
+    <div className="container" style={{textAlign: "center"}}>
+    <div className="row jumbotron">
+    <div className="display-4" style={{padding: "40px"}}>Profile Update</div>
+
+    <form style={{margin: "0 auto"}}>
+  <div className="form-group">
+  <div className="form-group">
+    <label htmlFor="exampleFormControlFile1">Example file input</label>
+    <input type="file" className="form-control-file" id="exampleFormControlFile1" />
+  </div>
+    <label htmlFor="exampleInputEmail1">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div className="form-group">
+    <label htmlFor="exampleInputPassword1">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="exampleFormControlInput1">Email address</label>
+    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+  </div>
+  <div className="form-row">
+    <div className="form-group col-md-6">
+      <label htmlFor="inputEmail4">Email</label>
+      <input type="email" className="form-control" id="inputEmail4" placeholder="Email" />
+    </div>
+    <div className="form-group col-md-6">
+      <label htmlFor="inputPassword4">Password</label>
+      <input type="password" className="form-control" id="inputPassword4" placeholder="Password" />
+    </div>
+  </div>
+  <div className="form-group">
+    <label htmlFor="inputAddress">Address</label>
+    <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="inputAddress2">Address 2</label>
+    <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+  </div>
+  <div className="form-row">
+    <div className="form-group col-md-6">
+      <label htmlFor="inputCity">City</label>
+      <input type="text" className="form-control" id="inputCity" />
+    </div>
+    <div className="form-group col-md-4">
+      <label htmlFor="inputState">State</label>
+      <select id="inputState" className="form-control">
+        <option selected>Choose...</option>
+        <option>...</option>
+      </select>
+    </div>
+    <div className="form-group col-md-2">
+      <label htmlFor="inputZip">Zip</label>
+      <input type="text" className="form-control" id="inputZip" />
+    </div>
+  </div>
+  <div className="form-group">
+    <div className="form-check">
+      <input className="form-check-input" type="checkbox" id="gridCheck" />
+      <label className="form-check-label" htmlFor="gridCheck">
+        Check me out
+      </label>
+    </div>
+  </div>
+  <div className="form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+  </div>
+  <div className="form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+  </div>
+  <div className="form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+  </div>
+  <div className="form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+  </div>
+  <div className="form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+  </div>
+  <div className="form-group">
+    <label htmlFor="exampleFormControlSelect1">Example select</label>
+    <select className="form-control" id="exampleFormControlSelect1">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <div className="form-group">
+    <label htmlFor="exampleFormControlSelect2">Example multiple select</label>
+    <select multiple className="form-control" id="exampleFormControlSelect2">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <div className="form-group">
+    <label htmlFor="exampleFormControlTextarea1">Example textarea</label>
+    <textarea className="form-control" id="exampleFormControlTextarea1" rows={3}></textarea>
+  </div>
+  <div className="form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+    </div>
+    </div>
   )
 }
 
