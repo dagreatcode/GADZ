c26b184c99bfba404dd124fe2e7cee351a0ee5f7
dagreatcode <dagreatcode@gmail.com>
2023-10-02 17:45:48 -0400

    added admin login page

--- client/src/containers/Admin/Admin.tsx
+++ client/src/containers/Admin/Admin.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Admin = () => {
   return (
     <>
       <div>Admin</div>
-      <Link to="/B2BMessages">B2B Messages</Link><br/>
-      <Link to="/Dash">DashBoard</Link><br/>
-      <Link to="/PrintOut">Print Outs</Link><br/>
-      <Link to="/TicketsCreated">User IT Tickets</Link><br/>
-      <Link to="/AdminAllUsers">All Users</Link><br/>
+      <Link to="/B2BMessages">B2B Messages</Link>
+      <br />
+      <Link to="/Dash">DashBoard</Link>
+      <br />
+      <Link to="/PrintOut">Print Outs</Link>
+      <br />
+      <Link to="/TicketsCreated">User IT Tickets</Link>
+      <br />
+      <Link to="/AdminAllUsers">All Users</Link>
+      <br />
     </>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
