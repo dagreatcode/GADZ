3a0d3a3360e788d9e98b59093ad779175503ad7b
Vincent Kendrick <dagreatcode@gmail.com>
2023-12-05 12:17:23 -0500

    added number button

--- client/src/containers/AboutUs/AboutUs.tsx
+++ client/src/containers/AboutUs/AboutUs.tsx
@@ -1,23 +1,42 @@
-import React from 'react'
-import Video1 from '../../components/Banner/Banner'
-import {Link} from 'react-router-dom'
+import React from "react";
+import Video1 from "../../components/Banner/Banner";
+import { Link } from "react-router-dom";
 
 function AboutUs() {
   return (
     <>
-        <div className="container" style={{textAlign: "center"}}>
-    <div className="row jumbotron">
-    <div className="display-4" style={{padding: "40px"}}>About Us</div>
-      <div>      
-        <div><Link to="/Bio">Biography</Link></div>
-        <div><Link to="/Consultation">Consultation</Link></div>
+      <div style={{ padding: "25px", textAlign: "center", top: "50%" }}>
+        <a
+          style={{ backgroundColor: "blue", borderRadius: "100px" }}
+          href="tel:(1)(9725978040)"
+          className="btn btn-dark btn-sm"
+          // tabindex="-1"
+          role="button"
+          aria-disabled="true"
+        >
+          <h1 style={{ fontSize: "50px" }}>972-597-8040</h1>
+        </a>
       </div>
-      <div>      
-        <Link to="/DispatchAgreement">Dispatch Agreement</Link><br/>
-        <Link to="/RoadFreight">Road Freight</Link>
-      </div>
-      <Video1 />
-      {/* <iframe 
+      <div className="container" style={{ textAlign: "center" }}>
+        <div className="row jumbotron">
+          <div className="display-4" style={{ padding: "40px" }}>
+            About Us
+          </div>
+          <div>
+            <div>
+              <Link to="/Bio">Biography</Link>
+            </div>
+            <div>
+              <Link to="/Consultation">Consultation</Link>
+            </div>
+          </div>
+          <div>
+            <Link to="/DispatchAgreement">Dispatch Agreement</Link>
+            <br />
+            <Link to="/RoadFreight">Road Freight</Link>
+          </div>
+          <Video1 />
+          {/* <iframe 
         width={1222} height={887}
         src="https://www.youtube.com/embed/ErOMbspikNg" 
         title="4K 3D Earth Globe Spinning" 
@@ -25,10 +44,10 @@ function AboutUs() {
         allow="accelerometer; autoPlay=true; clipboard-write; encrypted-media; gyroscope; picture-in-picture full" 
         allowFullScreen
       ></iframe> */}
+        </div>
       </div>
-      </div>
-    </>  
-  )
+    </>
+  );
 }
 
-export default AboutUs
+export default AboutUs;
