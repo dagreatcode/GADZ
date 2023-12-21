264a2d5bde136d85db33347a6a3ea926f2832543
Vincent Kendrick <dagreatcode@gmail.com>
2023-12-21 00:06:49 -0500

    Added more images to the pages

--- client/src/containers/AboutUs/globalassetdispatching/ConsultationForm/Consultation.tsx
+++ client/src/containers/AboutUs/globalassetdispatching/ConsultationForm/Consultation.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Image1 from './images/realMeets.jpeg'
 
 const Consultation = () => {
   return (
     <div className="container" style={{textAlign: "center"}}>
     <div className="row jumbotron">
     <div className="display-4" style={{padding: "40px"}}>Consultation</div>
+    <img src={Image1} style={{width:"100%"}}alt="talks" />
     </div>
     </div>
   )
