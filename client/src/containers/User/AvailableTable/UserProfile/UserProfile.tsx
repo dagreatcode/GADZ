cacf28c108e3883e8aa4e533048e1d41d1bb6e37
Vincent Kendrick <dagreatcode@gmail.com>
2023-12-24 09:57:08 -0500

    updated user profile page view

--- client/src/containers/User/AvailableTable/UserProfile/UserProfile.tsx
+++ client/src/containers/User/AvailableTable/UserProfile/UserProfile.tsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const UserProfile = () => {
+  // TODO: Add the api call to pull selected user profile
+
+  // FIXME: Fix this page to seem like its loading until the user profile data comes in.
+
   return (
     <>
       <div>UserProfile</div>
-      {/* 
-      * First an Last Name
-      * Customer since
-      * Type of Owner
-      * Available Status
-      * All Services Available
-      * Add Business Button to save
-      * Locations      
-      * * Message Customer
-      */}
-       <Link to="/MessageUser">Message user</Link><br/>
+      <h1>Available Status</h1>
+      <h1>Images</h1>
+      <h1>First an Last Name</h1>
+      <h1>Customer since</h1>
+      <h1>Type of Owner</h1>
+      <h1>All Services Available</h1>
+      <h1>Add Business Button to save</h1>
+      <h1>Locations</h1>
+      <h1> Message Customer</h1>
+      <Link to="/MessageUser">Message user</Link>
+      <br />
     </>
-  )
-}
+  );
+};
 
-export default UserProfile
+export default UserProfile;
