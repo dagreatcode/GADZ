302da8aad1f7e661a6670ea35db42cfec7f47d3c
Vincent Kendrick <dagreatcode@gmail.com>
2024-04-13 15:42:20 -0400

    working on live

--- client/src/containers/Admin/Agreements/Agreements.tsx
+++ client/src/containers/Admin/Agreements/Agreements.tsx
@@ -27,7 +27,7 @@ const Agreements = () => {
       letsClean();
     };
     // setAgreement(true);
-  }, [setData, setAgreement]);
+  }, [setData]);
 
   useEffect(() => {
     const letsClean = () => {
