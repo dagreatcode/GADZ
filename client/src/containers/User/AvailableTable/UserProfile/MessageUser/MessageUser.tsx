88869631a405f3f63f7dac9e6ddc6fb3560fb1a9
Vincent Kendrick <dagreatcode@gmail.com>
2024-09-20 21:14:10 -0400

    fixing the socket to hit the backend

--- client/src/containers/User/AvailableTable/UserProfile/MessageUser/MessageUser.tsx
+++ client/src/containers/User/AvailableTable/UserProfile/MessageUser/MessageUser.tsx
@@ -8,7 +8,7 @@ interface Message {
   content: string;
 }
 
-const socket = io("https://gadzconnect.com"); // Backend Socket.IO server
+const socket = io("https://gadzconnect.com/messages"); // Backend Socket.IO server
 // const socket = io("http://localhost:3002"); // Backend Socket.IO server
 
 const MessageUser: React.FC = () => {
