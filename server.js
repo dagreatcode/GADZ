14f3882b882b2366f080c7f752500ca393f386b3
Vincent Kendrick <dagreatcode@gmail.com>
2024-10-14 17:47:56 -0400

    updates

--- server.js
+++ server.js
@@ -62,91 +62,6 @@ app.use(require("./routes"));
 app.get("/api/loads", loadController.getAllLoads); // Get all loads
 app.post("/api/loads", loadController.createLoad); // Create a new load
 
-// const { Load, User } = require("./models"); // Adjust the import path as necessary
-
-// app.post("/loads", async (req, res) => {
-//   const { description, company, userId } = req.body;
-//   // Validate userId
-//   if (!userId) {
-//     console.log("logs", req.body);
-//     return res.status(400).json({ error: "User ID is required" });
-//   }
-
-//   try {
-//     // Check if the user exists
-//     const user = await User.findByPk(userId);
-//     if (!user) {
-//       return res.status(404).json({ error: "User does not exist." });
-//     }
-
-//     // Create load
-//     const newLoad = await Load.create({ description, company, userId });
-//     return res.status(201).json(newLoad);
-//   } catch (error) {
-//     console.error("Error creating load:", error);
-//     return res
-//       .status(500)
-//       .json({ error: "An error occurred while creating the load." });
-//   }
-// });
-
-// app.post("/api/myLoads", async (req, res) => {
-//   const { description, company, userId } = req.body;
-
-//   // // Check if user exists
-//   // const user = await db.User.findByPk(userId);
-//   // if (!user) {
-//   //   return res.status(400).json({ error: "User does not exist." });
-//   // }
-
-//   try {
-//     const newLoad = await db.Load.create({ description, company, userId });
-//     res.status(201).json(newLoad);
-//   } catch (error) {
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
-// FIXME: This works TODO: Delete this once not
-// // API endpoints for QR code creation and viewing
-// app.post("/api/qr-create", async (req, res) => {
-//   const data = req.body;
-//   try {
-//     const response = await axios.post(
-//       "https://hovercode.com/api/v2/hovercode/create/",
-//       data,
-//       {
-//         headers: {
-//           Authorization: `Token ${process.env.API_KEY_QR}`,
-//         },
-//         timeout: 10000,
-//       }
-//     );
-//     res.json(response.data);
-//   } catch (error) {
-//     console.error("Error posting QR:", error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
-
-// app.get("/api/qr-view", async (req, res) => {
-//   try {
-//     const response = await axios.get(
-//       "https://hovercode.com/api/v2/workspace/82140683-32bd-4422-9ff9-7ecec248c952/hovercodes/",
-//       {
-//         headers: {
-//           Authorization: `Token ${process.env.API_KEY_QR}`,
-//         },
-//         timeout: 10000,
-//       }
-//     );
-//     res.json(response.data);
-//   } catch (error) {
-//     console.error("Error fetching QR codes:", error);
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
-
 // // Retrieve loads ////////////////////////////////////
 const fetch = require("node-fetch");
 //
@@ -154,8 +69,12 @@ const clientID = process.env.CLIENT_ID;
 const token123 = process.env.TOKEN_123; // Authorization code
 const devURI = process.env.DEV_URI;
 const clientSecret = process.env.CLIENT_SECRET;
-const reToken123 = process.env.RE_TOKEN_123;
+const bearer123 = process.env.BEARER_123;
+const uri123 = process.env.URI_123;
 
+// ///////////////////////////////////////////////////
+// // Use Route to Retrieve an Auth Code
+// ///////////////////////////////////////////////////
 // Route to start the OAuth flow
 app.get("/authorize", async (req, res) => {
   const query = new URLSearchParams({
@@ -163,14 +82,18 @@ app.get("/authorize", async (req, res) => {
     client_id: clientID,
     redirect_uri: devURI,
     scope: "loadsearching",
-    state: "string",
+    state: "gadzconnect_dev",
     login_hint: "string",
   }).toString();
 
   // Redirect the user to the authorization URL
-  res.redirect(`https://api.dev.123loadboard.com/authorize?${query}`);
+  res.redirect(`${uri123}/authorize?${query}`);
+  // console.log();
 });
 
+// ///////////////////////////////////////////////////
+// // Use Auth Code to Retrieve an access token (and optionally a refresh token)
+// ///////////////////////////////////////////////////
 // Route to handle the redirect
 app.get("/auth/callback", async (req, res) => {
   const authCode = req.query.code; // Extract the authorization code from the query params
@@ -184,7 +107,7 @@ app.get("/auth/callback", async (req, res) => {
     redirect_uri: devURI,
   }).toString();
 
-  const tokenResp = await fetch("https://api.dev.123loadboard.com/token", {
+  const tokenResp = await fetch(`${uri123}/token`, {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
@@ -206,68 +129,15 @@ app.get("/auth/callback", async (req, res) => {
   );
 });
 
-// After you navigate to http://localhost:3000/authorize, the following steps should occur:
-
-// Authorization Redirect: Your browser will be redirected to the authorization page of the API. You'll need to log in (if not already logged in) and authorize the application to access your account.
-
-// Capture the Authorization Code:
-
-// Once you successfully authorize the application, the API will redirect you back to your specified redirect_uri (which should be something like http://localhost:3000/auth/callback).
-// In the address bar, you will see a query string appended to the URL, like this:
-// bash
-// Copy code
-// http://localhost:3000/auth/callback?code=your_authorization_code
-// The your_authorization_code is what you need to capture.
-// Using the Code:
-
-// The code is automatically captured by the server because the /auth/callback route in your Express application is set up to handle this.
-// The server logs the authorization code to the console, which allows you to see it:
-// css
-// Copy code
-// Authorization Code: your_authorization_code
-// Requesting an Access Token:
-
-// The server then uses this code to make a request to the API to exchange it for an access token.
-// The response containing the access token (and possibly a refresh token) will also be logged to the console:
-// css
-// Copy code
-// Access Token Response: { ... }
-// Next Steps:
-
-// With the access token, you can now make authorized API calls to fetch loads or other resources as needed.
-// You might want to store the access token securely (e.g., in memory, database, etc.) to use it in subsequent API requests.
-// Summary
-// Go to http://localhost:3000/authorize → log in and authorize.
-// Observe the auth/callback URL for the authorization code.
-// Check your server console for the access token response.
-
-// //
-// // Authorization
-// // async function run() {
-// //   const query = new URLSearchParams({
-// //     response_type: "code",
-// //     client_id: clientID,
-// //     redirect_uri: devURI,
-// //     scope: "loadsearching",
-// //     state: "string",
-// //     login_hint: "string",
-// //   }).toString();
-
-// //   const resp = await fetch(
-// //     `https://api.dev.123loadboard.com/authorize?${query}`,
-// //     {
-// //       method: "GET",
-// //       headers: { "User-Agent": "string" },
-// //     }
-// //   );
-
-// //   const data = await resp.text();
-// //   console.log(data);
-// // }
+//////////////////////////////////////////////////////
 
 // ///////////////////////////////////////////////////
-// // Use Auth Code to Retrieve an access token (and optionally a refresh token)
-// async function run() {
+// // Use Auth Code to Retrieve an access token (and optionally a refresh token) and make an POST /loads/search
+// // Main endpoint for searching for loads as a carrier. Get /loads/{id} is the only other route available.
+// ///////////////////////////////////////////////////
+// let bearerToken; // Variable to store the access token
+
+// async function getToken() {
 //   const formData = {
 //     grant_type: "authorization_code",
 //     code: token123,
@@ -275,7 +145,7 @@ app.get("/auth/callback", async (req, res) => {
 //     redirect_uri: devURI,
 //   };
 
-//   const resp = await fetch(`https://api.dev.123loadboard.com/token`, {
+//   const resp = await fetch(`${uri123}/token`, {
 //     method: "POST",
 //     headers: {
 //       "Content-Type": "application/x-www-form-urlencoded",
@@ -291,21 +161,38 @@ app.get("/auth/callback", async (req, res) => {
 //     body: new URLSearchParams(formData).toString(),
 //   });
 
-//   const data = await resp.text();
-//   console.log(data);
+//   const data = await resp.json();
+
+//   // Check if the response has an access token
+//   if (data.access_token) {
+//     bearerToken = data.access_token; // Save the token for later use
+//     console.log("Access Token:", bearerToken);
+//   } else {
+//     console.error("Access token not found in response:", data);
+//   }
+//   getLoads();
 // }
-// /////////////////////////////////////////////////////
-// // Use access code above in the bearer to make call
-// async function run() {
-//   const resp = await fetch(`https://api.dev.123loadboard.com/loads/search`, {
+
+// let bearerToken; // Variable to store the access token
+
+// // // //////////////////////////////////////////////////////////////////
+// // // Use access code above as the bearer ${bearerToken} to make call.
+// // ////////////////////////////////////////////////////////////////////
+// async function getLoads() {
+//   if (!bearerToken) {
+//     console.error("Bearer token is not available. Please run run2 first.");
+//     return;
+//   }
+
+//   const resp = await fetch(`${uri123}/loads/search`, {
 //     method: "POST",
 //     headers: {
+//       "123LB-Correlation-Id": "123GADZ",
 //       "Content-Type": "application/json",
 //       "123LB-Api-Version": "1.3",
 //       "User-Agent": "gadzconnect_dev",
 //       "123LB-AID": "Ba76be66d-dc2e-4045-87a3-adec3ae60eaf",
-//       Authorization:
-//         "bearer LHTciINa6gQSY5WLitZ8wESHA-j7EUW_-ixPOp_tHK24Ph73S92aw25EF-4VNWnCuDxQbzl5ypNe0uKqAgL9hc6xuCCjkLj0ujJfACGT8OIiuLA9ok_nvO9kCVqG03tgThj131jLDM4Z2d8l8Tsjs4scK0EoUonTAARLp5ztSxgk-LK7vUk36y9MPzvVBtbJOurDBSPJrZbEM17_K9thwCfz8wEcNMiMBZbp42FmhNQ1Sll8j1zTXcT_UF_VbJt0m0idHnWgd6HCUo04YMILKFC8lG7-5jrGrSnlj2g3xHBpgbujJdn9GM6swMxThY7jlLY2CXDphwHfTbv9JqOUnTgFvgv2_Vu5Jy0QiJkCNT4pPkmxHihDU4eoofWw2rrl1GhmnSGYIX2coGGIPuTvDY9JBR_Rtqs5HAWHFG9HlZ35BADQLYTRMocsh5I1EbRVrEI0Ug1Zw_kM8cIAlPVmw2QtL_R1wJOyhppRg_783feMPHYs_0fudEhMI-MP1GEVx5Dwv5YkGtEKQ3W0nbXI_GswhASyGZhG-Ayx-4Lb-RoVcMPBZfI4L2gottOLgXhdtuajccpHWpiOJ-jp5MBKQGG_bueytxD3KISBc_6YFdr6XHB793fL8OI-VHoeLDFadlSyCSNpgfeFgmxJm7jzTlUZDxFbhiz_DpdtpTE0edl02ntx2YzSdNHeplJNn1FMaax-XkrwJJEkUM3sppKfOL-WtP0VO1AS0UXK3sxIERHFZC4_KUqPXia0ZBb_h97WmVM5pms7G_0JzteRAdS3jbvzuesDw_VfQhjMFWOP9mE77z2jo1JB2BldQqMKyoiqd31_aZIQHso4vHBu9GFV_NLRm3RNd8-3pQVH0TR7XIAeX7MCqPPnvgX7Mra7zREjCylMu2Q8ITtLYJvmPIRpu0SgFgo7pgUwP_ZSbHktSUiglc_bNWolvUCbys78Ag_3",
+//       Authorization: `Bearer ${bearerToken}`,
 //     },
 //     body: JSON.stringify({
 //       metadata: {
@@ -324,23 +211,6 @@ app.get("/auth/callback", async (req, res) => {
 //       maxExtraDrops: 2147483647,
 //       hasTeam: true,
 //       hasRate: true,
-//       company: {
-//         name: "string",
-//         types: "None",
-//         minRating: 5,
-//         isFavorite: true,
-//         isFactorable: true,
-//         isTiaMember: true,
-//         isTiaCertified: true,
-//       },
-//       modifiedOnStart: "2019-08-24T14:15:22Z",
-//       modifiedOnEnd: "2019-08-24T14:15:22Z",
-//       minMileage: 32767,
-//       maxMileage: 32767,
-//       minOriginRadius: 0,
-//       isFavoriteBroker: true,
-//       minWeight: 0,
-//       minLength: 0,
 //       origin: {
 //         states: ["string"],
 //         city: "string",
@@ -372,7 +242,7 @@ app.get("/auth/callback", async (req, res) => {
 //   const data = await resp.json();
 //   console.log(data);
 // }
-//////////////////////////////////////////////////////
+// getToken();
 
 // Test routes
 app.get("/api/config", (req, res) => {
@@ -400,5 +270,3 @@ db.sequelize
   .catch((err) => {
     console.error("Error syncing database:", err.message);
   });
-
-// run();
