95e4f917da364508d2842b174d6449d60437e9d7
Vincent <dagreatcode@gmail.com>
2025-09-30 02:31:37 -0400

    updates

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,106 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
+import "./NavBar.css";
 
-const NavBar = () => {
+const NavBar: React.FC = () => {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const toggleNavbar = () => setIsCollapsed(!isCollapsed);
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link to="/" className="navbar-brand">
-          <img
-            src="./favicon-32x32.png"
-            width="45"
-            alt="GADZ Up"
-            className="d-inline-block align-middle mr-2"
-          />
+    <nav className="navbar-container">
+      <div className="navbar-inner">
+        <Link to="/" className="navbar-logo">
+          <img src="./favicon-32x32.png" alt="GADZ Up" className="logo-img" />
+          <span>GADZConnect</span>
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
+
+        {/* Hamburger Button */}
+        <button className="navbar-toggler" onClick={toggleNavbar}>
+          <span className="hamburger-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
+
+        {/* Collapsible Links */}
+        <div className={`navbar-links-container ${isCollapsed ? "collapsed" : ""}`}>
+          <ul className="navbar-links">
+            <li>
               <NavLink to="/AboutUs" className="nav-link">
                 About Us
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink to="/ContactUs" className="nav-link">
-                Contact Us
-              </NavLink>
-            </li>
-            <li className="nav-item">
+            <li>
               <NavLink to="/Services" className="nav-link">
                 Services
               </NavLink>
             </li>
-            <li className="nav-item">
+            <li>
+              <NavLink to="/ContactUs" className="nav-link">
+                Contact Us
+              </NavLink>
+            </li>
+            <li>
               <NavLink to="/Login" className="nav-link">
                 Login
               </NavLink>
             </li>
           </ul>
-        </div>
-        <div className="social-links">
-          <span>
-            <a
-              aria-label="Twitter Link"
-              href="https://www.twitter.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i
-                className="fab fa-twitter"
-                style={{ fontSize: "30px", padding: "20px" }}
-              ></i>
+
+          <div className="navbar-social">
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-twitter"></i>
             </a>
-          </span>
-          <span>
-            <a
-              aria-label="Facebook Link"
-              href="https://www.facebook.com/global.asset.dispatching.service"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i
-                className="fab fa-facebook-f"
-                style={{ fontSize: "30px", padding: "20px" }}
-              ></i>
+            <a href="https://facebook.com/global.asset.dispatching.service" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-facebook-f"></i>
             </a>
-          </span>
-          <span>
-            <a
-              aria-label="YouTube Link"
-              href="https://www.youtube.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i
-                className="fab fa-youtube"
-                style={{ fontSize: "30px", padding: "20px" }}
-              ></i>
+            <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-youtube"></i>
             </a>
-          </span>
-          <span>
-            <a
-              aria-label="Instagram Link"
-              href="https://www.instagram.com/Global_assetleader/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i
-                className="fab fa-instagram"
-                style={{ fontSize: "30px", padding: "20px" }}
-              ></i>
+            <a href="https://instagram.com/Global_assetleader/" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-instagram"></i>
             </a>
-          </span>
+          </div>
         </div>
       </div>
     </nav>
